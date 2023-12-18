5c1f4a150396f9b0b2d4c5bf8e41c3edf7fb40d8
Marcello <marcello.faugiana@kyklos-group.com>
2023-12-18 13:04:58 +0100

    Added equals and GetHasCode to Point2D

--- GeometryTest/Geometry.Core/Points/Point2D.cs
+++ GeometryTest/Geometry.Core/Points/Point2D.cs
@@ -1,10 +1,52 @@
 ﻿namespace Geometry.Core.Points;
 
-public class Point2D
+public class Point2D : IEquatable<Point2D>
 {
     private double _x;
     private double _y;
 
+
     public override string ToString() =>
         $"({_x}, {_y})";
+
+    public override bool Equals(object? obj) =>
+        Equals(obj as Point2D);
+
+
+    public bool Equals(Point2D? other)
+    {
+        if (other is null)
+            return false;
+
+        if (ReferenceEquals(this, other)) 
+            return true;
+
+        if (_x == other._x && _y == other._y)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public override int GetHashCode() =>
+        HashCode.Combine(_x, _y);
+
+
+
+
+
+    /*
+     *         1 1 0 0 1 0 1
+     *         1 0 0 0 1 1 0
+     *         
+     *         
+     * &       1 0 0 0 1 0 0     
+     * |       1 1 0 0 1 1 1
+     * ^       1 0 1 1 1 0 0
+     * 
+     * 
+     */
+
+
 }
--- GeometryTest/GeometryConsole/Program.cs
+++ GeometryTest/GeometryConsole/Program.cs
@@ -1,4 +1,62 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Geometry.Core.Points;
+using System.Security.Cryptography;
 
-Console.WriteLine("Hello, World!");
+void PrintToConsole(object? item)
+{
+	if (item is null)
+	{
+		Console.WriteLine("Passed argument is null!");
+	}
+	else
+	{
+		Console.WriteLine($"{item} is of type {item.GetType().FullName}");
+	}
+}
+
+
+object p1 = new Point2D();
+object p2 = new Point2D();
+object p3 = p1;
+
+PrintToConsole(p1);
+PrintToConsole(p2);
+
+Console.WriteLine(p1.Equals(p2));
+
+List<object> list = new List<object>();
+
+list.Add(p1);
+list.Add(p2);
+list.Add(p3);
+
+bool isContained = list.Contains(p1);
+
+
+
+
+
+//PrintToConsole(p1);	
+
+//p1 = DateTime.Today;
+//PrintToConsole(p1);
+
+//p1 = new object();
+//PrintToConsole(p1);
+
+//p1 = "Ciao Mondo!";
+//PrintToConsole(p1);
+
+
+//PrintToConsole(p2);
+
+//PrintToConsole(null);
+
+
+
+
+
+//DateTime d1 = DateTime.Now;
+
+//Console.WriteLine($"{d1.ToString("yyyy-MM-ddTHH:mm:ss.fff")} is of type {d1.GetType().FullName}");
+
