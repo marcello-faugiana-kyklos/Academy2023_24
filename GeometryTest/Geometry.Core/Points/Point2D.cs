ddf61a35d4db6b81697499c17f3a6d611d3a83e7
Marcello <marcello.faugiana@kyklos-group.com>
2023-12-19 13:00:33 +0100

    Completed the class Point2D

--- GeometryTest/Geometry.Core/Points/Point2D.cs
+++ GeometryTest/Geometry.Core/Points/Point2D.cs
@@ -6,6 +6,69 @@ public class Point2D : IEquatable<Point2D>
     private double _y;
 
 
+    private class Point2DZero : Point2D
+    {
+        public override void SetCoordinates(double x, double y)
+        {
+        }
+
+        public override double X 
+        { 
+            get => base.X; 
+            set { } 
+        }
+
+        public override double Y
+        {
+            get => base.Y;
+            set { }
+        }
+    }
+
+    public static readonly Point2D Zero = 
+        new Point2DZero();
+    
+    //private static Point2D Zero1 => new Point2D();
+    
+    //private static Point2D Zero2 { get; }
+
+    //static Point2D()
+    //{
+    //    Zero2 = new Point2D();
+    //}
+
+
+    public Point2D() : this(0d, 0d)
+    {
+    }
+
+    public Point2D(double x, double y)
+    {
+        _x = x;
+        _y = y;
+    }
+
+    public static Point2D NewFromX(double x) =>
+        new Point2D(x, 0);
+
+    public static Point2D NewFromY(double y) =>
+        new Point2D(0, y);
+
+    public virtual double X
+    {
+        get => _x;
+        set => _x = value;
+    }
+
+    public virtual double Y
+    {
+        get => _y;
+        set => _y = value;
+    }
+
+    public virtual void SetCoordinates(double x, double y) =>
+        (_x, _y) = (x, y);
+
     public override string ToString() =>
         $"({_x}, {_y})";
 
@@ -20,20 +83,12 @@ public class Point2D : IEquatable<Point2D>
 
         if (ReferenceEquals(this, other)) 
             return true;
-
-        if (_x == other._x && _y == other._y)
-        {
-            return true;
-        }
-
-        return false;
+ 
+        return _x == other._x && _y == other._y;
     }
 
     public override int GetHashCode() =>
         HashCode.Combine(_x, _y);
-
-
-
 
 
     /*
@@ -41,12 +96,34 @@ public class Point2D : IEquatable<Point2D>
      *         1 0 0 0 1 1 0
      *         
      *         
-     * &       1 0 0 0 1 0 0     
-     * |       1 1 0 0 1 1 1
-     * ^       1 0 1 1 1 0 0
+     * &       1 0 0 0 1 0 0     (AND logico bit a bit)
+     * |       1 1 0 0 1 1 1     (OR logico bit a bit)
+     * ^       0 1 0 0 0 1 1     (XOR logico bit a bit)
      * 
      * 
      */
 
+
+    // DRY Don't Repeat Yourself
+    // KISS Keep It Simple Stupid
+
+    public double DistanceFromOrigin() =>
+        DistanceFromOtherPoint(Zero);
+
+
+    public double DistanceFromOtherPoint(Point2D other)
+    {
+        if (other is null)
+        {
+            throw new ArgumentNullException(nameof(other));
+        }
+
+        double sumOfSquaresOfDifferences = 
+            Math.Pow(X - other.X, 2)
+            +
+            Math.Pow(Y - other.Y, 2);
+
+        return Math.Sqrt(sumOfSquaresOfDifferences);
+    }
 
 }
--- GeometryTest/GeometryConsole/Program.cs
+++ GeometryTest/GeometryConsole/Program.cs
@@ -15,8 +15,60 @@ void PrintToConsole(object? item)
 }
 
 
-object p1 = new Point2D();
-object p2 = new Point2D();
+Point2D p1 = new Point2D();
+object zero_o = Point2D.Zero;
+Console.WriteLine(p1.Equals(zero_o));
+
+//for (int i = 0; i < 100_000_000; i++)
+//{
+//    p1.DistanceFromOrigin();
+//}
+
+Console.WriteLine(p1.DistanceFromOrigin());
+var zero = Point2D.Zero;
+zero.X = 2;
+zero.Y = 3;
+Point2D.Zero.SetCoordinates(8, 10);
+Console.WriteLine(p1.DistanceFromOrigin());
+
+
+
+Point2D p4 = Point2D.NewFromX(3);
+Point2D p5 = Point2D.NewFromY(5);
+Console.WriteLine(p5.DistanceFromOtherPoint(p1));
+Console.WriteLine(p1.DistanceFromOtherPoint(p5));
+
+p1 = null;
+double distance_5_1;
+try
+{
+    distance_5_1 = p5.DistanceFromOtherPoint(p1);
+}
+catch (Exception ex)
+{
+	distance_5_1 = 0;
+}
+
+
+
+//try
+//{
+//    Console.WriteLine(p5.DistanceFromOtherPoint(null));
+//}
+//catch (ArgumentNullException ex)
+//{
+//	Console.WriteLine($"{ex.Message}");
+//}
+
+
+double x_of_p1 = p1.X;
+
+p1.SetCoordinates(6, 9);
+p1.X = 9;
+p1.Y = 7;
+x_of_p1 = p1.X;
+
+object p2 = new Point2D(3, 4);
 object p3 = p1;
 
 PrintToConsole(p1);
@@ -24,13 +76,13 @@ PrintToConsole(p2);
 
 Console.WriteLine(p1.Equals(p2));
 
-List<object> list = new List<object>();
+//List<object> list = new List<object>();
 
-list.Add(p1);
-list.Add(p2);
-list.Add(p3);
+//list.Add(p1);
+//list.Add(p2);
+//list.Add(p3);
 
-bool isContained = list.Contains(p1);
+//bool isContained = list.Contains(p1);
 
 
 
@@ -60,3 +112,6 @@ bool isContained = list.Contains(p1);
 
 //Console.WriteLine($"{d1.ToString("yyyy-MM-ddTHH:mm:ss.fff")} is of type {d1.GetType().FullName}");
 
+
+
+// TDD Test Driven Development
